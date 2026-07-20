ebdacecbee57bd5c451d39cb9d8a415618bdd828
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:35:10 +0200

    Update ldpc_pkg.sv

--- rtl/ldpc_pkg.sv
+++ rtl/ldpc_pkg.sv
@@ -13,14 +13,10 @@ package ldpc_pkg;
 
     parameter int M = 10;
     parameter int N = 20;
-
     parameter int MAX_ITER = 10;
-
     parameter int LLR_W = 8;
-
     parameter int MAX_CN_DEG = 4;
     parameter int MAX_VN_DEG = 2;
-
     parameter int EDGE_COUNT = 40;
 
     //----------------------------------------------------------
@@ -28,23 +24,17 @@ package ldpc_pkg;
     //----------------------------------------------------------
 
     typedef logic signed [LLR_W-1:0] llr_t;
-
     typedef logic [N-1:0] codeword_t;
-
     typedef logic [M-1:0] syndrome_t;
-
     typedef logic [$clog2(MAX_CN_DEG)-1:0] cn_edge_t;
-
     typedef logic [$clog2(MAX_VN_DEG)-1:0] vn_edge_t;
-
     typedef logic [$clog2(MAX_ITER+1)-1:0] iter_t;
 
     //----------------------------------------------------------
     // FSM
     //----------------------------------------------------------
 
-    typedef enum logic [2:0]
-    {
+    typedef enum logic [2:0]{
         ST_RESET,
         ST_INIT,
         ST_CN_UPDATE,
@@ -59,28 +49,20 @@ package ldpc_pkg;
     //----------------------------------------------------------
 
     localparam llr_t LLR_ZERO = '0;
-
     localparam llr_t LLR_MAX = 8'sd127;
-
     localparam llr_t LLR_MIN = -8'sd128;
 
     //----------------------------------------------------------
     // Saturation
     //----------------------------------------------------------
 
-    function automatic llr_t sat_llr
-    (
+    function automatic llr_t sat_llr(
         input logic signed [15:0] value
     );
 
-        if(value > LLR_MAX)
-            sat_llr = LLR_MAX;
-
-        else if(value < LLR_MIN)
-            sat_llr = LLR_MIN;
-
-        else
-            sat_llr = value[LLR_W-1:0];
+        if(value > LLR_MAX)			sat_llr = LLR_MAX;
+        else if(value < LLR_MIN)	sat_llr = LLR_MIN;
+        else						sat_llr = value[LLR_W-1:0];
 
     endfunction
 
@@ -88,15 +70,12 @@ package ldpc_pkg;
     // Absolute Value
     //----------------------------------------------------------
 
-    function automatic llr_t abs_llr
-    (
+    function automatic llr_t abs_llr(
         input llr_t value
     );
 
-        if(value[LLR_W-1])
-            abs_llr = -value;
-        else
-            abs_llr = value;
+        if(value[LLR_W-1])	abs_llr = -value;
+        else				abs_llr = value;
 
     endfunction
 
@@ -104,8 +83,7 @@ package ldpc_pkg;
     // Sign
     //----------------------------------------------------------
 
-    function automatic logic sign_llr
-    (
+    function automatic logic sign_llr(
         input llr_t value
     );
 
@@ -117,16 +95,13 @@ package ldpc_pkg;
     // Min
     //----------------------------------------------------------
 
-    function automatic llr_t min_llr
-    (
+    function automatic llr_t min_llr(
         input llr_t a,
         input llr_t b
     );
 
-        if(a < b)
-            min_llr = a;
-        else
-            min_llr = b;
+        if(a < b)	min_llr = a;
+        else		min_llr = b;
 
     endfunction
 
@@ -140,10 +115,8 @@ package ldpc_pkg;
         input llr_t b
     );
 
-        if(a > b)
-            max_llr = a;
-        else
-            max_llr = b;
+        if(a > b)	max_llr = a;
+        else		max_llr = b;
 
     endfunction
 
