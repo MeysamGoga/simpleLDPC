cf9281abd6aaeff03479c457d0774531af543625
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:41:52 +0200

    Update llr_quantizer.sv

--- sim/llr_quantizer.sv
+++ sim/llr_quantizer.sv
@@ -8,37 +8,25 @@
 module llr_quantizer
 (
     input  real channel_value,
-
     output logic signed [7:0] llr
 );
 
     real scale;
     integer temp;
 
-
-    initial
-    begin
+    initial begin
         scale = 32.0;
     end
 
-
-    always @(*)
-    begin
+    always @(*) begin
 
         temp = $rtoi(channel_value * scale);
 
-
-        if(temp > 127)
-            llr = 8'sd127;
-
-        else if(temp < -128)
-            llr = -8'sd128;
-
-        else
-            llr = temp[7:0];
+        if(temp > 127)			llr = 8'sd127;
+        else if(temp < -128)	llr = -8'sd128;
+        else					llr = temp[7:0];
 
     end
-
 
 endmodule
 
