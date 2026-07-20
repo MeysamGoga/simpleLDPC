b37ed5f9ad8a787b306ee14227a44b3aa640ab50
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:13:21 +0200

    Update ldpc_cn_update.sv

--- rtl/ldpc_cn_update.sv
+++ rtl/ldpc_cn_update.sv
@@ -12,12 +12,8 @@ module ldpc_cn_update
 
     input  logic start,
     output logic done,
-
-    input  ldpc_pkg::llr_t vn_to_cn
-        [ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
-
-    output ldpc_pkg::llr_t cn_to_vn
-        [ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG]
+    input  ldpc_pkg::llr_t vn_to_cn [ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
+    output ldpc_pkg::llr_t cn_to_vn	[ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG]
 );
 
     import ldpc_pkg::*;
@@ -33,11 +29,9 @@ module ldpc_cn_update
     logic parity_sign;
     logic out_sign;
 
-    always_ff @(posedge clk or posedge rst)
-    begin
+    always_ff @(posedge clk or posedge rst) begin
 
-        if(rst)
-        begin
+        if(rst) begin
 
             done <= 1'b0;
 
@@ -45,73 +39,46 @@ module ldpc_cn_update
                 for(e=0;e<MAX_CN_DEG;e=e+1)
                     cn_to_vn[cn][e] <= '0;
 
-        end
-
-        else
-        begin
+        end else begin
 
             done <= 1'b0;
 
-            if(start)
-            begin
+            if(start) begin
+                for(cn=0;cn<M;cn=cn+1) begin
 
-                for(cn=0;cn<M;cn=cn+1)
-                begin
-
-                    //--------------------------------------------------
                     // Find parity sign
-                    //--------------------------------------------------
-
                     parity_sign = 1'b0;
 
                     for(e=0;e<CN_DEGREE[cn];e=e+1)
                         parity_sign ^= sign_llr(vn_to_cn[cn][e]);
 
-                    //--------------------------------------------------
                     // Find first and second minimum
-                    //--------------------------------------------------
-
                     min1 = LLR_MAX;
                     min2 = LLR_MAX;
                     min_index = 0;
 
-                    for(e=0;e<CN_DEGREE[cn];e=e+1)
-                    begin
+                    for(e=0;e<CN_DEGREE[cn];e=e+1) begin
 
                         mag = abs_llr(vn_to_cn[cn][e]);
 
-                        if(mag < min1)
-                        begin
-                            min2 = min1;
+                        if(mag < min1) begin		min2 = min1;
                             min1 = mag;
                             min_index = e;
                         end
-                        else if(mag < min2)
-                        begin
-                            min2 = mag;
-                        end
+                        else if(mag < min2) begin	min2 = mag;	end
 
                     end
 
-                    //--------------------------------------------------
                     // Generate CN messages
-                    //--------------------------------------------------
+                    for(e=0;e<CN_DEGREE[cn];e=e+1) begin
 
-                    for(e=0;e<CN_DEGREE[cn];e=e+1)
-                    begin
+                        if(e == min_index)	mag = min2;
+                        else				mag = min1;
 
-                        if(e == min_index)
-                            mag = min2;
-                        else
-                            mag = min1;
+                        out_sign = parity_sign ^ sign_llr(vn_to_cn[cn][e]);
 
-                        out_sign = parity_sign ^
-                                   sign_llr(vn_to_cn[cn][e]);
-
-                        if(out_sign)
-                            cn_to_vn[cn][e] <= -mag;
-                        else
-                            cn_to_vn[cn][e] <= mag;
+                        if(out_sign)	cn_to_vn[cn][e] <= -mag;
+                        else			cn_to_vn[cn][e] <= mag;
 
                     end
 
