4b4a52a086905471b5e6d1a334ecf530267d805d
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 15:28:01 +0200

    Create llr_quantizer.sv

---
+++ sim/llr_quantizer.sv
@@ -0,0 +1,45 @@
+//==============================================================
+// File : sim/llr_quantizer.sv
+//==============================================================
+
+`timescale 1ns/1ps
+`default_nettype none
+
+module llr_quantizer
+(
+    input  real channel_value,
+
+    output logic signed [7:0] llr
+);
+
+    real scale;
+    integer temp;
+
+
+    initial
+    begin
+        scale = 32.0;
+    end
+
+
+    always @(*)
+    begin
+
+        temp = $rtoi(channel_value * scale);
+
+
+        if(temp > 127)
+            llr = 8'sd127;
+
+        else if(temp < -128)
+            llr = -8'sd128;
+
+        else
+            llr = temp[7:0];
+
+    end
+
+
+endmodule
+
+`default_nettype wire
