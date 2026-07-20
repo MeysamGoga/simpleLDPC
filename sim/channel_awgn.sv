ac010096040dd9e8b047af79c969bd91b8513ec8
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:40:30 +0200

    Update channel_awgn.sv

--- sim/channel_awgn.sv
+++ sim/channel_awgn.sv
@@ -10,40 +10,28 @@ module channel_awgn
     input  logic clk,
 
     input  logic enable,
-
     input  logic tx_bit,
-
     output real channel_output
 );
 
     real noise;
     real sigma;
 
-    initial
-    begin
+    initial begin
         sigma = 0.7;
     end
 
+    always @(posedge clk) begin
 
-    always @(posedge clk)
-    begin
+        if(enable) begin
 
-        if(enable)
-        begin
-
-            if(tx_bit == 1'b0)
-                channel_output = 1.0;
-            else
-                channel_output = -1.0;
-
+            if(tx_bit == 1'b0)	channel_output = 1.0;
+            else				channel_output = -1.0;
 
             noise = $itor($urandom_range(0,10000))/10000.0;
-
             noise = (noise-0.5)*2.0*sigma;
 
-
-            channel_output =
-                channel_output + noise;
+            channel_output = channel_output + noise;
 
         end
 
