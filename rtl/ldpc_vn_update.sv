27d51753a9f45d48eb4c21180714944d9057dfee
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:08:01 +0200

    Update ldpc_vn_update.sv

--- rtl/ldpc_vn_update.sv
+++ rtl/ldpc_vn_update.sv
@@ -13,17 +13,10 @@ module ldpc_vn_update
     input  logic start,
     output logic done,
 
-    input  ldpc_pkg::llr_t channel_llr
-        [ldpc_pkg::N],
-
-    input  ldpc_pkg::llr_t cn_to_vn
-        [ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
-
-    output ldpc_pkg::llr_t vn_to_cn
-        [ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
-
-    output ldpc_pkg::llr_t app_llr
-        [ldpc_pkg::N]
+    input  ldpc_pkg::llr_t channel_llr	[ldpc_pkg::N],
+    input  ldpc_pkg::llr_t cn_to_vn		[ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
+    output ldpc_pkg::llr_t vn_to_cn		[ldpc_pkg::M][ldpc_pkg::MAX_CN_DEG],
+    output ldpc_pkg::llr_t app_llr		[ldpc_pkg::N]
 );
 
     import ldpc_pkg::*;
@@ -48,13 +41,11 @@ module ldpc_vn_update
     );
 
         int i;
-
+		
         begin
-
             edge_index = -1;
 
-            for(i=0;i<CN_DEGREE[cn_id];i=i+1)
-            begin
+            for(i=0;i<CN_DEGREE[cn_id];i=i+1) begin
                 if(CN_CONN[cn_id][i]==vn_id)
                     edge_index=i;
             end
@@ -65,16 +56,11 @@ module ldpc_vn_update
 
     //----------------------------------------------------------
 
-    always_ff @(posedge clk or posedge rst)
-    begin
+    always_ff @(posedge clk or posedge rst) begin
 
-        if(rst)
-        begin
-
+        if(rst) begin
             done <= 1'b0;
-
-            for(vn=0;vn<N;vn=vn+1)
-                app_llr[vn] <= '0;
+            for(vn=0;vn<N;vn=vn+1)	app_llr[vn] <= '0;
 
             for(cn=0;cn<M;cn=cn+1)
                 for(edge=0;edge<MAX_CN_DEG;edge=edge+1)
@@ -82,89 +68,40 @@ module ldpc_vn_update
 
         end
 
-        else
-        begin
+        else begin
 
             done <= 1'b0;
-
-            if(start)
-            begin
-
-                //--------------------------------------------------
+            if(start) begin
                 // APP LLR
-                //--------------------------------------------------
-
-                for(vn=0;vn<N;vn=vn+1)
-                begin
-
+                for(vn=0;vn<N;vn=vn+1) begin
                     sum = channel_llr[vn];
-
-                    for(k=0;k<VN_DEGREE[vn];k=k+1)
-                    begin
-
+                    for(k=0;k<VN_DEGREE[vn];k=k+1) begin
                         cn = VN_CONN[vn][k];
-
                         edge = edge_index(cn,vn);
-
                         sum += cn_to_vn[cn][edge];
-
                     end
-
                     app_llr[vn] <= sat_llr(sum);
-
                 end
 
-                //--------------------------------------------------
                 // Extrinsic messages
-                //--------------------------------------------------
+                for(vn=0;vn<N;vn=vn+1) begin
 
-                for(vn=0;vn<N;vn=vn+1)
-                begin
-
-                    for(k=0;k<VN_DEGREE[vn];k=k+1)
-                    begin
-
+                    for(k=0;k<VN_DEGREE[vn];k=k+1) begin
                         cn = VN_CONN[vn][k];
-
                         edge = edge_index(cn,vn);
-
                         sum = channel_llr[vn];
-
-                        for(int j=0;j<VN_DEGREE[vn];j=j+1)
-                        begin
-
-                            if(j!=k)
-                            begin
-
-                                edge2 = edge_index(
-                                            VN_CONN[vn][j],
-                                            vn
-                                        );
-
-                                sum += cn_to_vn
-                                [
-                                    VN_CONN[vn][j]
-                                ]
-                                [
-                                    edge2
-                                ];
-
+                        for(int j=0;j<VN_DEGREE[vn];j=j+1) begin
+                            if(j!=k) begin
+                                edge2 = edge_index(VN_CONN[vn][j],vn);
+                                sum += cn_to_vn[VN_CONN[vn][j]][edge2];
                             end
-
                         end
-
                         vn_to_cn[cn][edge] <= sat_llr(sum);
-
                     end
-
                 end
-
                 done <= 1'b1;
-
             end
-
         end
-
     end
 
 endmodule
