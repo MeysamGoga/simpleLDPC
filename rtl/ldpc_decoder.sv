4b0f22fb4ced904a7886de71b645a3b8ae7e3f9b
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 16:08:56 +0200

    Update ldpc_decoder.sv

--- rtl/ldpc_decoder.sv
+++ rtl/ldpc_decoder.sv
@@ -11,14 +11,10 @@ module ldpc_decoder
     input  logic rst,
 
     input  logic start,
-
     output logic done,
     output logic success,
-
     output ldpc_pkg::iter_t iter_count,
-
-    output logic decoded_bits
-        [ldpc_pkg::N]
+    output logic decoded_bits [ldpc_pkg::N]
 );
 
     import ldpc_pkg::*;
@@ -27,13 +23,10 @@ module ldpc_decoder
     // Internal memories
     //----------------------------------------------------------
 
-    llr_t channel_llr [N];
-
-    llr_t vn_to_cn [M][MAX_CN_DEG];
-
-    llr_t cn_to_vn [M][MAX_CN_DEG];
-
-    llr_t app_llr [N];
+    llr_t channel_llr	[N];
+    llr_t vn_to_cn		[M][MAX_CN_DEG];
+    llr_t cn_to_vn		[M][MAX_CN_DEG];
+    llr_t app_llr		[N];
 
     //----------------------------------------------------------
     // Control signals
@@ -49,26 +42,20 @@ module ldpc_decoder
     logic vn_done;
     logic syndrome_done;
 
-
     //----------------------------------------------------------
     // Test Channel Initialization
     //----------------------------------------------------------
 
     integer i;
 
-    always_ff @(posedge clk or posedge rst)
-    begin
+    always_ff @(posedge clk or posedge rst) begin
 
-        if(rst)
-        begin
-
+        if(rst) begin
             for(i=0;i<N;i=i+1)
                 channel_llr[i] <= '0;
-
         end
 
-        else if(init)
-        begin
+        else if(init) begin
 
             channel_llr[0]  <=  8'sd42;
             channel_llr[1]  <=  8'sd38;
@@ -98,7 +85,6 @@ module ldpc_decoder
 
     end
 
-
     //----------------------------------------------------------
     // Check Node Update
     //----------------------------------------------------------
@@ -110,11 +96,9 @@ module ldpc_decoder
 
         .start(cn_start),
         .done(cn_done),
-
         .vn_to_cn(vn_to_cn),
         .cn_to_vn(cn_to_vn)
     );
-
 
     //----------------------------------------------------------
     // Variable Node Update
@@ -127,16 +111,11 @@ module ldpc_decoder
 
         .start(vn_start),
         .done(vn_done),
-
         .channel_llr(channel_llr),
-
-        .cn_to_vn(cn_to_vn),
-
-        .vn_to_cn(vn_to_cn),
-
+        .cn_to_vn(cn_to_vn),	// input
+        .vn_to_cn(vn_to_cn),	// output
         .app_llr(app_llr)
     );
-
 
     //----------------------------------------------------------
     // Syndrome Check
@@ -148,16 +127,11 @@ module ldpc_decoder
         .rst(rst),
 
         .start(syndrome_start),
-
         .app_llr(app_llr),
-
         .done(syndrome_done),
-
         .success(success),
-
         .hard_bits(decoded_bits)
     );
-
 
     //----------------------------------------------------------
     // FSM Controller
@@ -169,25 +143,17 @@ module ldpc_decoder
         .rst(rst),
 
         .start(start),
-
         .cn_done(cn_done),
         .vn_done(vn_done),
-
         .syndrome_done(syndrome_done),
-
         .success(success),
-
         .init(init),
-
         .cn_start(cn_start),
         .vn_start(vn_start),
         .syndrome_start(syndrome_start),
-
         .done(done),
-
         .iter_count(iter_count)
     );
-
 
 endmodule
 
