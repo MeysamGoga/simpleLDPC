8dbfc609c0c38e2be1f92d4ed6d74672b805a0c0
MeysamGoga <mseyrafi@bsc.es>
2026-07-20 15:49:24 +0200

    Update ldpc_top.sv

--- rtl/ldpc_top.sv
+++ rtl/ldpc_top.sv
@@ -9,14 +9,11 @@ module ldpc_top
 (
     input  logic clk,
     input  logic rst,
-
+	
     input  logic start,
-
     output logic done,
     output logic success,
-
     output logic [ldpc_pkg::N-1:0] decoded_bits,
-
     output ldpc_pkg::iter_t iter_count
 );
 
@@ -28,41 +25,28 @@ module ldpc_top
 
     logic decoded_bits_array [N];
 
-
     ldpc_decoder u_decoder
     (
         .clk(clk),
         .rst(rst),
-
         .start(start),
-
         .done(done),
-
         .success(success),
-
         .iter_count(iter_count),
-
         .decoded_bits(decoded_bits_array)
     );
-
 
     //----------------------------------------------------------
     // Convert unpacked to packed output
     //----------------------------------------------------------
 
     genvar i;
-
     generate
-
         for(i=0;i<N;i=i+1)
         begin
-
             assign decoded_bits[i] = decoded_bits_array[i];
-
         end
-
     endgenerate
-
 
 endmodule
 
